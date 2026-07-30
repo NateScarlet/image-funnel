b082864fcb0f939bfa88bef68e4621e368050f93
NateScarlet <NateScarlet@Gmail.com>
2026-07-31 01:31:05 +0800

    chore: remove unexpected log form queries

--- internal/application/pairing/pairing_requests.go
+++ internal/application/pairing/pairing_requests.go
@@ -3,28 +3,9 @@ package pairing
 import (
 	"context"
 	"main/internal/shared"
-	"time"
-
-	"go.uber.org/zap"
 )
 
 func (h *Handler) PairingRequests(ctx context.Context) (dtos []*shared.PairingRequestDTO, err error) {
-	startTime := time.Now()
-
-	defer func() {
-		if err != nil {
-			h.logger.Error("pairing requests failed",
-				zap.Duration("duration", time.Since(startTime)),
-				zap.Error(err),
-			)
-		} else {
-			h.logger.Info("did pairing requests",
-				zap.Int("count", len(dtos)),
-				zap.Duration("duration", time.Since(startTime)),
-			)
-		}
-	}()
-
 	var results []*shared.PairingRequestDTO
 	for pr, err := range h.pairingSvc.Find(ctx) {
 		if err != nil {
